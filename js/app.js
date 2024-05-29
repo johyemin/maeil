95e6aef65ceb9ec8f8437ee62e221b4d99301222
johyemin <49671771+johyemin@users.noreply.github.com>
2024-05-29 15:27:26 +0900

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,17 @@
+$(function (){
+    $(".slider-single").slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        fade: true,
+        useTransform: true,
+        asNavFor: ".slider-nav"
+      });
+      $(".slider-nav").slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        asNavFor: ".slider-single",
+        dots: true,
+        focusOnSelect: true
+      });
+})
