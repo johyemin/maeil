b31092547fb54db2d86de8b90fee0b314329dcfc
johyemin <49671771+johyemin@users.noreply.github.com>
2024-06-05 17:02:58 +0900

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,17 +1,50 @@
 $(function (){
-    $(".slider-single").slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        arrows: true,
-        fade: true,
-        useTransform: true,
-        asNavFor: ".slider-nav"
-      });
-      $(".slider-nav").slick({
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        asNavFor: ".slider-single",
-        dots: true,
-        focusOnSelect: true
-      });
+  // ham 바
+    $(".ham").click(function(){
+      $(".submenu").show()
+    })
+
+    $(".close").click(function(){
+      $(".submenu").hide()
+    })
+
+    // top
+    $(window).scroll(function(){
+    
+      if( $(this).scrollTop() > 100 ){
+        $(".top").addClass("on");
+      }
+      else{
+        $(".top").removeClass("on");
+      }
+      
+    });
+      
+  $(".top").click(function(){
+    window.scrollTo({top : 0, behavior: 'smooth'}); 
+  });
+
+
+  $(".c1").click(function(){
+    $(".fix_i img").attr('src', 'image/brand_maeil.jpg')
+  })
+
+  $(".c2").click(function(){
+    $(".fix_i img").attr('src', 'image/brand_bio.jpg')
+  })
+
+  $(".c3").click(function(){
+    $(".fix_i img").attr('src', 'image/brand_absolute.jpg')
+  })
+
+  $(".c4").click(function(){
+    $(".fix_i img").attr('src', 'image/brand_coffee.jpg')
+  })
+
+  $(".c5").click(function(){
+    $(".fix_i img").attr('src', 'image/brand_amazing.jpg')
+  })
+
+
+    
 })
